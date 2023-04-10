64c1cb9512d133b914215d2b3affcc250891aa06
msk821 <misaki.nakamura1@gmail.com>
2023-04-11 01:15:09 +0900

    発展問題1,2

--- 01_テキスト/01_JavaScriptの関数/main.js
+++ 01_テキスト/01_JavaScriptの関数/main.js
@@ -16,7 +16,7 @@ for (let i = 1; i <= number; i++) {
   }
 }*/
 
-const genkiFunction = function (number) {
+/*const genkiFunction = function (number) {
   for (let i = 1; i <= number; i++) {
     if (i % 3 === 0) {
       console.log(i + "!!!!!!!!")
@@ -24,7 +24,65 @@ const genkiFunction = function (number) {
       console.log(i)
     }
   }
+}*/
+
+const FizzBuzz = function (number) {
+  for (let i = 1; i <= number; i++) {
+    if (i % 15 === 0) {
+      console.log("FizzBuzz")
+    } else if (i % 5 === 0) {
+      console.log("Buzz")
+    } else if (i % 3 === 0) {
+      console.log("Fizz")
+    } else {
+      console.log(i)
+    }
+  }
 }
+
+const genkifun = function (number) {
+  for (let i = 1; i <= number; i++) {
+    if (/3/.test(i) === true) {
+      console.log(i + "!!!!!!")
+    } else {
+      console.log(i)
+    }
+  }
+}
+
+/*let count = 0
+
+const countUp = function () {
+  // count を更新
+  // count += 1
+  // // count を秒単位にして表示
+  // count = count / 100
+  count += 1
+
+  if(count % 3000 === 0){
+    console.log(count / 100 + "!!!!!!!!!")
+  }
+  //console.log(`${count % 3000 === 0 ? count / 100 + "!" : count / 100}`)
+}
+
+const countfunction = function (n) {
+  // for (let i = 0; i <= n; setInterval(countUp, 10)) {
+  //   if (count % 3000) {
+  //     console.log(count + "!!!!!!")
+  //   } else {
+  //     console.log(count)
+  //   }
+  // }
+  const clearId = setInterval(() => {
+    countUp()
+    if (count >= 1000 * n) clearInterval(clearId)
+  }, 10)
+}*/
+
+/*const genkifunction = function(n){
+  for(i = 0; i <= n; i++)
+}*/
+
 // 練習問題 １
 //const plus1 = function (x) {
 //  return x + 1
